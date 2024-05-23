cd659d0645c4b9b5d37578fb1c6f73464598ae2b
Alexander Brown <alex.brown7711@gmail.com>
2024-05-23 12:50:20 -0600

    restructuring

--- lua/core/core.lua
+++ lua/core/core.lua
@@ -1,20 +1,8 @@
 -- Turn on terminal gui colors
 vim.opt.termguicolors = true
 
--- Higlight current line
-vim.opt.cursorline = true
-
--- Highlight paired braces
-vim.opt.showmatch = true
-
 -- Vertical Bar
 vim.opt.colorcolumn = "81"
-
--- Tabs instead of spaces
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
-vim.opt.expandtab = true
 
 -- Enable mouse support
 vim.opt.mouse = "a"
@@ -22,12 +10,7 @@ vim.opt.mouse = "a"
 -- Cleanup completion menu (move to lsp?)
 vim.opt.completeopt = "menuone,noselect"
 
--- Display whitespace
-vim.opt.listchars = "tab:¦ ,trail:·,extends:>,precedes:<"
-vim.opt.list = true
-
 -- Searching
-
 vim.opt.path = vim.opt.path + "**"
 vim.opt.wildmenu = true
 vim.opt.ignorecase = true
---
+++ lua/lang.lua
@@ -0,0 +1,20 @@
+--===============================================================================
+-- Common Text File Configuration
+--===============================================================================
+
+-- Tabs instead of spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+-- Display whitespace
+vim.opt.listchars = "tab:¦ ,trail:·,extends:>,precedes:<"
+vim.opt.list = true
+
+-- Higlight current line
+vim.opt.cursorline = true
+
+-- Highlight paired braces
+vim.opt.showmatch = true
+
