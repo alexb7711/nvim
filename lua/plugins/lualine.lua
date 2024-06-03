c2fe6b5893680ee0e6b76f386a70b8f401efdfdb
Alexander Brown <alex.brown7711@gmail.com>
2024-06-02 19:41:06 -0600

    update, added soil

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,18 +1,21 @@
---- Everforest -- -- https://github.com/nvim-lualine/lualine.nvim -- --
+-- Lualine
+--
+-- https://github.com/nvim-lualine/lualine.nvim -- --
+--
 
 --------------------------------------------------------------------------------
 -- LAZY SETUP
 ---------------------------------------------------------------------------------
 local lualine = {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' }
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 }
 
 ---------------------------------------------------------------------------------
 -- CONFIGURATION
 ---------------------------------------------------------------------------------
 function lualine.config()
-  require('lualine').setup()
+  require("lualine").setup()
 end
 
 return lualine
